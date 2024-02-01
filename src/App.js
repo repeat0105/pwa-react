2c7b7407f5fd94d51a8120a804a1d8240b9832e9
repeat0105 <kkyzxc40@gmail.com>
2024-02-01 11:41:20 +0900

    first1

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  useEffect(() => {
+    if (Notification.permission != 'granted') {
+      // console.log(Notification.permission)
+      Notification.requestPermission().then()
+
+    }
+  }, []) 
+
+
+  function msg() {
+    navigator.serviceWorker.controller.postMessage({
+      message:'Hello, service worker!'
+    })
+  }
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={msg}>알림메세지</button>
+
+
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
